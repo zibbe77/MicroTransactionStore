ab6a37968cbae63464cc2d77ad380cdd5d640557
Theo zibell <theo@zibell.se>
2021-11-16 14:06:14 +0100

    first

---
+++ MicroTransactionStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroTransactionStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
